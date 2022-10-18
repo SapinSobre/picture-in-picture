e1281ddaa84d7cea14c73285760b970f89847419
Nathalie Daoût <nat.dao3@gmail.com>
2022-10-18 22:21:52 +0200

    play video

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const button = document.getElementById('button');
 
 async function selectMediaStream() {
     try {
-        const mediaStream = navigator.mediaDevices.getDisplayMedia();
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
         videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
     } catch(error) {
         // here the error is catched...
         console.log("Oooops... There is an error!", error);
